c2023347fd6c0def572650212d80782db909c4ab
Brian Lee <32053403+bobolee1239@users.noreply.github.com>
2018-09-26 23:02:42 +0800

    Add files via upload

--- Game.cpp
+++ Game.cpp
@@ -153,7 +153,7 @@ void HungryHippo::updateView(bool isEating){
     // reset to space 
     for(int i=0; i<N+2; i++)
         for (int j=0; j<N+2; j++)
-            field[i][j] = (isInField(i, j))?'-':' ';
+            field[i][j] = (isInField(i, j))?' ':'=';
    
         
     // update ball position 
